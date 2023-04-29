d90833bec0c16de69a2f9683c1fc7b0b21009c48
Adrian <122322238+ADomingo8@users.noreply.github.com>
2023-04-28 21:01:44 -0400

    Added new cart html, images, changed where the chekout is, changed the app.py a little, and added photos to starting data

--- database/startingData.sql
+++ database/startingData.sql
@@ -11,19 +11,64 @@ INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_n
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Apples', 'An edible cultivation of the Malus genus.', 2.00, 100, 'static/images/apple.jpeg', 'Fruit');
+VALUES ('The Dark Side of the Moon', '1973 Pink Floyd Album', 25.99, 60, 'static/images/The_Dark_Side_of_the_Moon.jpg', 'Rock');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Bananas', 'A long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.', 1.00, 100, 'static/images/banana.jpeg', 'Fruit');
+VALUES ('...And Justice for All', '1988 Metallica Album', 39.99, 10, 'static/images/justiceforall.jpeg', 'Rock');
 
 INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
-VALUES ('Mangos', 'The best fruit on the planet.', 4.00, 100, 'static/images/mango.jpeg', 'Fruit');
+VALUES ('In Utero', '1993 Nirvana Album', 39.99, 5, 'static/images/in-utero.jpg', 'Rock');
 
-INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
-VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Hotel California', '1976 Eagles Album', 35.99, 10, 'static/images/Hotelcalifornia.jpg', 'Rock');
 
-INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
-VALUES ('2', 'dritchie', '2', 10, '2022-12-21 7:30:30', 5.50);
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('OK Computer', '1997 Radiohead Album', 19.99, 20, 'static/images/OKCOMPUTER.jpeg', 'Rock');
 
-INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
-VALUES ('3', 'llamport', '3', 10, '2022-12-21 7:30:30', 5.50);
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Synchronicity', '1983 The Police Album', 35.99, 15, 'static/images/synchronicity.jpg', 'Rock');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Freudian', '2017 Daniel Caeasar Album', 29.99, 10, 'static/images/freudian.jpg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('channel ORANGE', '2012 Frank Ocean Album', 49.99, 5, 'static/images/channelorange.jpeg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Apollo XXI', '2019 Steve Lacy Album', 39.99, 30, 'static/images/Aollo-XXI.jpeg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Ivory', '2022 Omar Apollo ', 29.99, 10, 'static/images/ivroy.jpeg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Candydrip', '2022 Lucky Daye Album', 19.99, 10, 'static/images/candydrip.jpeg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('If Orange Was A Place', '2021 Tems Album', 25.99, 10, 'static/images/orange-was-a-place.jpeg', 'R&B');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('The Melodic Blue', '2021 Baby Keem Album', 35.99, 10, 'static/images/the-melodic-blue.jpeg', 'Rap');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Nothing Was The Same', '2013 Drake Album', 40.99, 30, 'static/images/nothingwasthesame.jpeg', 'Rap');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('TESTING', '2018 A$AP Rocky Album', 39.99, 10, 'static/images/testing.jpeg', 'Rap');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('CALL ME IF YOU GET LOST', '2021 Tyler, The Creator Album', 45.99, 10, 'static/images/callmeifyougetlost.jpg', 'Rap');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('HEROS & VILLAINS', '2022 Metro Boomin Album', 25.99, 10, 'static/images/heros&villains.jpeg', 'Rap');
+
+INSERT into `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
+VALUES ('Madvillainy', '2004 MF DOOM', 35.99, 10, 'static/images/madvillainy.jpeg', 'Rap');
+
+INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `cost`)
+VALUES ('1', 'aturing', '1', 10, 5.50);
+
+INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `cost`)
+VALUES ('2', 'dritchie', '2', 10, 5.50);
+
+INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `cost`)
+VALUES ('3', 'llamport', '3', 10, 5.50);
